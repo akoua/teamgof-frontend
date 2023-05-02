1574e8dc7aff1880ca326c59c581b992c7a24c3e
Lubin Fritzgy <fritzgy.lubin@etudiant.univ-rennes1.fr>
2023-05-02 20:50:04 +0200

    chore: add demo project

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <h1>Let's Go TEAMGOF 🔥</h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "Accueil - TEAMGOF",
+});
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #f1f1f1;
+  font-weight: bold;
+  height: 100vh;
+}
+</style>
